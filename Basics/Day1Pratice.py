6aaeae7eada9d2a4347def9b5368a43d6949a598
Krishna Ningaraddi <krishna.ningaraddi08@gmail.com>
2025-07-20 00:10:40 +0530

    Day 2

--- Basics/Day1Pratice.py
+++ Basics/Day1Pratice.py
@@ -1,1 +1,47 @@
+
+
 print("Hello hackers!")
+
+x = 10
+y = 20
+z = 25
+
+# Vairables
+
+students_count = 1000
+rating = 4.99
+a = True
+course_name = "Python Programming is easy"
+
+print(x)
+print(y)
+print(z)
+
+# Strings
+
+topic = "python basics"
+
+message = """
+I am krishna and i am  learning python
+Hey Hackers!!
+"""
+
+# Built in functions
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+
+# Conditional Statements
+
+if x == y:
+    print("Hello World!")
+elif x > y:
+    print("Hello chai lovers")
+else:
+    print("Hello hackers")
+
+
+def hello():
+    print("Hello Hackers")
