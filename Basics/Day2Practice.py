a08e22b4859f216fdd5e12c1900744fe5c905259
Krishna Ningaraddi <krishna.ningaraddi08@gmail.com>
2025-07-23 17:33:04 +0530

    behind the scene working for python

--- Basics/Day2Practice.py
+++ Basics/Day2Practice.py
@@ -28,3 +28,65 @@ print(string1+" "+String2)
 string3 = string1+" "+String2
 
 print(string1[1:4])
+
+# Type of data will be assigned to the allocated memory not the variable example a=10 here 10 will be stored in the
+# memory and that memory allocation will be of number type so on and so forth
+
+a = "krishna"
+
+# here the value of a will be changed to krishna but back of the box garbage collector will not replace it
+# immediately
+# for numbers and string it will be bit late as compared to other datatypes
+
+print(type(a))
+
+# Here the value of l1 is changed and hence l2 is also changed because both are pointing to same reference
+
+l1 = [1, 2, 3]
+l2 = l1
+
+print(l1)
+print(l2)
+
+l1[0] = 22
+
+print(l1)
+print(l2)
+
+# Here both p1 and p2 are pointing to the same reference but i have changed the reference of p2 with the same values
+# even though values are same in both p1 and p2 the reference is different any changes made in p1 will not affect the p2 and
+# vice versa
+
+p1 = [1, 2, 3]
+p2 = p1
+
+p2 = [1, 2, 3]
+
+print(p1)
+print(p2)
+
+p1[0] = 33
+
+print(p1)
+print(p2)
+
+# here initially both m and n are pointing to same reference so "==" and "is" both are true
+# but when we explicitly give the new reference of same values as in m to n then "==" will be true but "is" will be false
+# because both are pointing to different references as "=="checks value and "is" checks reference
+
+m = [1, 2, 3]
+n = m
+
+print(m)
+print(n)
+
+print(m == n)  # True
+print(m is n)  # True
+
+n = [1, 2, 3]
+
+print(m)
+print(n)
+
+print(m == n)  # True
+print(m is n)  # False
